2b14743300216cd2f58d2aab7119afbdccb1749e
AnshikaSharma24 <sharmarekha1072@gmail.com>
2021-10-15 19:22:33 +0530

    my first java project

---
+++ VirtualKeyProject/src/main/java/services/ScreenService.java
@@ -0,0 +1,34 @@
+package services;
+
+import screens.screenoption;
+import screens.Screen;
+import screens.WelcomeScreen;
+import system.Directory;
+
+
+public class ScreenService {
+	
+	//static Directory dir = new Directory();
+	
+	
+	
+	public static WelcomeScreen WelcomeScreen = new WelcomeScreen();
+    public static screenoption screenoption = new screenoption();
+    
+    
+
+    public static Screen CurrentScreen = WelcomeScreen;
+
+    
+    public static Screen getCurrentScreen() {
+        return CurrentScreen;
+    }
+
+    
+    public static void setCurrentScreen(Screen currentScreen) {
+        CurrentScreen = currentScreen;
+    }
+    
+    
+    
+}
---
+++ VirtualKeyProject/src/main/java/virtualkeyapp/Application.java
@@ -0,0 +1,13 @@
+package virtualkeyapp;
+import screens.WelcomeScreen;
+
+public class Application {
+
+    public static void main(String[] args) {
+    	
+    	WelcomeScreen welcome = new WelcomeScreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+
+    }
+}
